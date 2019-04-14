4dc7785254c7b8326287f5d991ddc29bae0c8475
1llum1na71 <dylansflin7@hotmail.com>
2019-04-14 00:52:35 -0500

    Added mockup for manage view

--- views/manage.php
+++ views/manage.php
@@ -5,21 +5,59 @@
 <div class="container pt-80">
 	<div class="container col-12 justify-content-center" style="margin-top: 20px; background-color: white;">
 		<div class="row">
-			<div class="col-12 col-sm-12 col-md-6">
-				<div class="card">
-					<div class="menu-display display-overlay">
-						<div class="menu-item-container"></div>
-						<div class="summary-box">
-							<p><span>Total: </span><span class="total-amt">$0.00</span></p>
+			<div class="col-12">
+				<div class="card mb-10">
+					<h4>Profits and Labor statistics</h4>
+					<hr>
+					<div class="row card-item">
+						<div class="col-7">
+							<p>Vendor 1</p>
+						</div>
+						<div class="col-5 text-right">
+							<a href="" class="btn btn-danger">Remove</a>
+						</div>
+					</div>
+				</div>
+				<div class="card mb-10">
+					<h4>Logged in workers</h4>
+					<hr>
+					<div class="row card-item">
+						<div class="col-5">
+							<p>Worker 1</p>
+						</div>
+						<div class="col-7 text-right">
+							<a href="" class="btn btn-primary">Clock-In</a>
+							<a href="" class="btn btn-danger">Clock-Out</a>
+						</div>
+					</div>
+				</div>
+				<div class="card mb-10">
+					<h4>Vendor List</h4>
+					<hr>
+					<div class="row card-item">
+						<div class="col-12">
+							<p>Vendor 1</p>
+						</div>
+					</div>
+				</div>
+				<div class="card mb-10">
+					<h4>Supply Orders</h4>
+					<hr>
+					<div class="row card-item" style="background-color:white;border:1px solid #ccc;">
+						<div class="col-12">
+							<p>Order 1</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-12 col-sm-12 col-md-6">
-				<div class="card menu-btns"></div>
-			</div>
 		</div>
 	</div>
 </div>
+<style>
+	.card-item p {
+		line-height: 40px;
+		margin-bottom: 0px;
+	}
+</style>
 
 <?php include 'layouts/close_page.html'?>
